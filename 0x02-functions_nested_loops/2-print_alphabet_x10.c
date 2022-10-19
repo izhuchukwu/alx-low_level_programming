13b041e0caa1794e582c5cf5ac24f2fb67103577
izhuchukhukwu <izhuchuchukwu@gmail.com>
2022-10-19 03:58:28 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints out the alphabet
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+char i;
+int x;
+x = 0;
+
+while (x < 10)
+{
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+x++;
+}
+}
